b980c716ef962359d83c3bffb3ce0c935c399e03
raihanadi073 <raihanadi073@gmail.com>
2024-11-30 12:05:00 +0700

    Add Modul 11 Multi Linked List

---
+++ Modul_11_MultiLinkedList/guided/guided.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct ChildNode
+{
+    string info;
+    ChildNode* next;
+};
+
+struct ParentNode
+{
+    string info;
+    ParentNode* next;
+    ChildNode* childHead;
+};
+
+ParentNode *createParent(string info)
+{
+    ParentNode *newNode = new ParentNode;
+    newNode->info = info;
+    newNode->childHead = NULL;
+    newNode->next = NULL;
+    return newNode;
+}
+
+ChildNode *createChild(string info)
+{
+    ChildNode *newNode = new ChildNode;
+    newNode->info = info;
+    newNode->next = NULL;
+    return newNode;
+}
+
+void insertParent(ParentNode *&head, string info)
+{
+    ParentNode *newNode = createParent(info);
+    if (head == NULL)
+    {
+        head = newNode;
+    }
+    else
+    {
+        ParentNode *temp = head;
+        while (temp->next != NULL)
+        {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void insertChild(ParentNode *head, string parenInfo, string childInfo)
+{
+    ParentNode *p = head;
+    while (p != NULL && p->info != parenInfo)
+    {
+        p = p->next;
+    }
+    if (p != NULL)
+    {
+        ChildNode *newChild = createChild(childInfo);
+        if (p->childHead == NULL)
+        {
+            p->childHead = newChild;
+        }
+        else
+        {
+            ChildNode *c = p->childHead;
+            while (c->next != NULL)
+            {
+                c = c->next;
+            }
+            c->next = newChild;
+        }
+    }
+}
+
+void printAll(ParentNode *head)
+{
+    ParentNode *p = head;
+    while (p != NULL)
+    {
+        cout << p ->info;
+        ChildNode *c = p->childHead;
+        if (c != NULL)
+        {
+            while (c != NULL)
+            {
+                cout << " -> " << c->info;
+                c = c->next;
+            }
+        }
+        cout << endl;
+        p = p->next;
+    }
+}
+
+int main()
+{
+    ParentNode *list = NULL;
+
+    insertParent(list, "Parent Node 1");
+    insertParent(list, "Parent Node 2");
+
+    printAll(list);
+    cout << "\n";
+    
+    insertChild(list, "Parent Node 1", "Child1.A");
+    insertChild(list, "Parent Node 1", "Child1.B");
+    insertChild(list, "Parent Node 2", "Child2.C");
+
+    printAll(list);
+
+    return 0;
+}
